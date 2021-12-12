2473fe417b59f7a49d88dd80d9fae3bb314d5497
gabriellenoelle <88753098+gabriellenoelle@users.noreply.github.com>
2021-12-11 23:30:45 -0500

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,9 +1,8 @@
-const apiRoutes = require("./routes/apiRoutes");
 const express = require("express");
 const router = express.Router();
-app.use("/api", apiRoutes);
-router.use(require("./partyRoutes"));
 
 router.use(require("./candidateRoutes"));
+router.use(require("./partyRoutes"));
+router.use(require("./voterRoutes"));
 
 module.exports = router;
--- routes/apiRoutes/partyRoutes.js
+++ routes/apiRoutes/partyRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const db = require("../../db/connection");
 
 // Get all parties
-
 router.get("/parties", (req, res) => {
   const sql = `SELECT * FROM parties`;
+
   db.query(sql, (err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message });
@@ -18,11 +18,11 @@ router.get("/parties", (req, res) => {
   });
 });
 
-//Get a specific party according to id
-
+// Get single party
 router.get("/party/:id", (req, res) => {
   const sql = `SELECT * FROM parties WHERE id = ?`;
   const params = [req.params.id];
+
   db.query(sql, params, (err, row) => {
     if (err) {
       res.status(400).json({ error: err.message });
@@ -36,14 +36,13 @@ router.get("/party/:id", (req, res) => {
 });
 
 // Delete a party
-
 router.delete("/party/:id", (req, res) => {
   const sql = `DELETE FROM parties WHERE id = ?`;
   const params = [req.params.id];
+
   db.query(sql, params, (err, result) => {
     if (err) {
       res.status(400).json({ error: res.message });
-      // checks if anything was deleted
     } else if (!result.affectedRows) {
       res.json({
         message: "Party not found",
